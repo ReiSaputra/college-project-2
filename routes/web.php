a9b681301ffc38c47f083c95d2b96e01028902e5
muhrizky435 <muhrizky435@gmail.com>
2024-04-16 10:33:36 +0700

    participant

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('landingPage');
+    return view('layouts.landingPage');
 })->name("landing");
 
 Route::fallback(function () {
